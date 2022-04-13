8a97e381a7fa106ff4580066fab4a017be7bc590
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2022-04-13 19:03:32 -0300

    Update Products
    Products wil  read the properties allocate in "DATA" and will display this on the screen

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,14 +1,41 @@
 import React from 'react'
-
-
+import { NavLink } from 'react-router-dom';
+import DATA from '../Data'
 
 const Product = () => {
 
+    const cardItem = (item) => {
         return (
-            <div ></div>
-                
+            <div class="card my-5 py-4" key={item.id} style={{width: "18rem"}}>
+                <img src={item.img} class="card-img-top" alt={item.name}/>
+                    <div class="card-body text-center">
+                        <h5 class="card-title">{item.name}</h5>
+                        <p className="lead">R$ {item.price}</p>
+                        <NavLink to={`/products/${item.id}`} class="btn btn-outline-primary">Buy Now</NavLink>
+                    </div>
+                    
        
-
+</div>
                 );
     }
-  export default Product
+
+                return (
+                <div>
+                    <div className="container py-5">
+                        <div className="row">
+                            <div className="col-12 text-center">
+                                <h1>Products</h1>
+                                <hr />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="container">
+                        <div className="row justify-content-around">
+                            {DATA.map(cardItem)}
+                        </div>
+                    </div>
+                </div>
+                )
+}
+
+                export default Product
