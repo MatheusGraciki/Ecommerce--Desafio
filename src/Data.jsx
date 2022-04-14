1d6596b9083d1fc54d8b04ed5236df396205d304
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2022-04-13 21:48:37 -0300

    fixing directories

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,71 +1,71 @@
 const DATA = [
-  {
-    "id": 312,
-    "name": "Super Mario Odyssey",
-    "price": 197.88,
-    "score": 100,
-    "img": "/assets/images/products/super-mario-odyssey.png"
-  },
-  {
-    "id": 201,
-    "name": "Call Of Duty Infinite Warfare",
-    "price": 49.99,
-    "score": 80,
-    "img": "/assets/images/products/call-of-duty-infinite-warfare.png"
-  },
-  {
-    "id": 102,
-    "name": "The Witcher III Wild Hunt",
-    "price": 119.5,
-    "score": 250,
-    "img": "/assets/images/products/the-witcher-iii-wild-hunt.png"
-  },
-  {
-    "id": 99,
-    "name": "Call Of Duty WWII",
-    "price": 249.99,
-    "score": 205,
-    "img": "/assets/images/products/call-of-duty-wwii.png"
-  },
-  {
-    "id": 12,
-    "name": "Mortal Kombat XL",
-    "price": 69.99,
-    "score": 150,
-    "img": "/assets/images/products/mortal-kombat-xl.png"
-  },
-  {
-    "id": 74,
-    "name": "Shards of Darkness",
-    "price": 71.94,
-    "score": 400,
-    "img": "/assets/images/products/shards-of-darkness.png"
-  },
-  {
-    "id": 31,
-    "name": "Terra Média: Sombras de Mordor",
-    "price": 79.99,
-    "score": 50,
-    "img": "/assets/images/products/terra-media-sombras-de-mordor.png"
-  },
-  {
-    "id": 420,
-    "name": "FIFA 18",
-    "price": 195.39,
-    "score": 325,
-    "img": "/assets/images/products/fifa-18.png"
-  },
-  {
-    "id": 501,
-    "name": "Horizon Zero Dawn",
-    "price": 115.8,
-    "score": 290,
-    "img": "/assets/images/products/horizon-zero-dawn.png"
-  }
-]
-
-      
-      
-
-
-export default DATA;
+    {
+      "id": 312,
+      "name": "Super Mario Odyssey",
+      "price": 197.88,
+      "score": 100,
+      "img": "/assets/images/products/super-mario-odyssey.png"
+    },
+    {
+      "id": 201,
+      "name": "Call Of Duty Infinite Warfare",
+      "price": 49.99,
+      "score": 80,
+      "img": "/assets/images/products/call-of-duty-infinite-warfare.png"
+    },
+    {
+      "id": 102,
+      "name": "The Witcher III Wild Hunt",
+      "price": 119.5,
+      "score": 250,
+      "img": "/assets/images/products/the-witcher-iii-wild-hunt.png"
+    },
+    {
+      "id": 99,
+      "name": "Call Of Duty WWII",
+      "price": 249.99,
+      "score": 205,
+      "img": "/assets/images/products/call-of-duty-wwii.png"
+    },
+    {
+      "id": 12,
+      "name": "Mortal Kombat XL",
+      "price": 69.99,
+      "score": 150,
+      "img": "/assets/images/products/mortal-kombat-xl.png"
+    },
+    {
+      "id": 74,
+      "name": "Shards of Darkness",
+      "price": 71.94,
+      "score": 400,
+      "img": "/assets/images/products/shards-of-darkness.png"
+    },
+    {
+      "id": 31,
+      "name": "Terra Média: Sombras de Mordor",
+      "price": 79.99,
+      "score": 50,
+      "img": "/assets/images/products/terra-media-sombras-de-mordor.png"
+    },
+    {
+      "id": 420,
+      "name": "FIFA 18",
+      "price": 195.39,
+      "score": 325,
+      "img": "/assets/images/products/fifa-18.png"
+    },
+    {
+      "id": 501,
+      "name": "Horizon Zero Dawn",
+      "price": 115.8,
+      "score": 290,
+      "img": "/assets/images/products/horizon-zero-dawn.png"
+    }
+  ]
+  
+        
+        
+  
+  
+  export default DATA;
