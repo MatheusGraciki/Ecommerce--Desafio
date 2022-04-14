b8d95e153cc1ac0101fbc7777b8d2ed05becc775
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2022-04-13 21:26:23 -0300

    Fix routes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import About from './components/About'
 import Product from './components/Product'
 import Contact from './components/Contact'
 import { Redirect, Route, Switch } from 'react-router-dom';
-
+import ProductDetail from './components/ProductDetail';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout'
 
 function App() {
   return (
@@ -14,7 +16,10 @@ function App() {
       <Header/>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/products" component={Product} />  
+        <Route exact path="/products" component={Product} />
+        <Route exact path="/products/:id" component={ProductDetail} />
+        <Route exact path="/cart" component={Cart} />
+        <Route exact path="/checkout" component={Checkout} />
         <Route exact path="/about" component={About} />
         <Route exact path="/contact" component={Contact} />
         <Redirect to="/" />
